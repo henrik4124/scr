7706b5870b9b3476c48d0ffa45a0f9e6304252c6
henrik4124 <44812275+henrik4124@users.noreply.github.com>
2021-03-08 18:51:14 +0100

    Add files via upload

--- 4124.sh
+++ 4124.sh
@@ -5,6 +5,285 @@
 beta=4124-beta
 offs=4124
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+nc=`tput sgr0`
+
+
+meny(){
+clear
+sleep 2
+
+echo "${green}"
+echo "              | || |/_ |__ \| || |                "
+echo "              | || |_| |  ) | || |_               "
+echo "              |__   _| | / /|__   _|              "
+echo "                 | | | |/ /_   | |            	"
+echo "                 |_| |_|____|  |_|                "  
+echo "                 ____    ________                 "
+echo "                /_   |  /   __   \                "
+echo "                 |   |  \____    /                "
+echo "                 |   |     /    /                 "
+echo "                 |___| /\ /____/                  "
+echo "                       \/                         "
+echo "${nc}"
+echo "${blue}" "                4124 script ${nc} ${green} 1.9.0       ${nc}"
+echo "             ${blue}    Welcome Back${nc} ${green} $USER ${nc}"
+
+echo "1) VPN"
+echo "2) nmap scan"
+echo "3) dir search"
+echo "4) Quit"
+
+	menyvalg
+
+}
+
+menyvalg(){
+
+read svar
+	
+	if [ $svar == 1 ];
+		
+		then
+
+			clear
+
+            	echo "VPN choosen"
+            	cd $mappe
+            	cd kali-anonsurf
+            	echo "Turn VPN off or on?"
+
+            	read VPN
+
+            	if [ $VPN == on ];
+            
+        		    then
+
+            			sudo anonsurf start
+
+            			echo "VPN is on"
+            			
+            			echo " "
+        				echo "returning to main menu"
+
+            			sleep 5
+
+            			meny
+
+        			else
+
+        				sudo anonsurf stop
+
+        				echo "VPN is now turned off"
+        				
+        				echo " "
+        				echo "returning to main menu"
+        				
+        				sleep 5
+
+        				meny
+
+        		fi
+    fi
+
+
+    if [ $svar == 2 ];
+
+    	then
+
+            clear
+            nmap
+   fi
+
+
+   if [ $svar == 3 ];
+
+   		then
+
+   			echo "directory search is choosen"
+            sleep 2
+
+            echo "Select ip adress (with port) to scan: "
+            read IP
+
+            cd $mappe
+            cd dirsearch
+
+            python3 dirsearch.py -e dir -u $IP
+
+            echo " "
+        	echo "returning to main menu"
+
+            sleep 5
+
+            meny
+   fi
+
+   if [ $svar == 4 ];
+
+   		then
+  	 	
+  	 		clear
+
+			echo "                                                          "
+			echo " _____         _            _                 _         _ "
+			echo "|_   ____ ___ |_|___ ___   | |___ _ _ ___ ___| |_ ___ _| |"
+			echo "  | ||  _| . || | .'|   |  | | .'| | |   |  _|   | -_| . |"
+			echo "  |_||_| |____| |__,|_|_|  |_|__,|___|_|_|___|_|_|___|___|"
+			echo "            |___|                                         ""                                                                                                                                                         "
+			echo "                   Enjoy                                  "
+
+			sleep 5
+
+			clear
+
+            exit
+
+  fi
+
+}
+
+
+nmapm(){
+
+		clear
+            echo "    ___  __   _____   ___        _   _ ___  ___  ___  ______ "
+			echo "   /   |/  | / __  \ /   |      | \ | ||  \/  | / _ \ | ___ \ " 
+			echo "  / /| |\`| | \`' / /'/ /| |___   |  \| || .  . |/ /_\ \| |_/ / "
+			echo " / /_| | | |   / / / /_| / __|  | . \` || |\/| ||  _  ||  __/ "
+			echo " \___  |_| |_./ /__\___  \__ \  | |\  || |  | || | | || |    "
+			echo "     |_/\___/\_____/   |_/___/  \_| \_/\_|  |_/\_| |_/\_|    "
+			echo "                                                             "
+			echo "                                                            "
+            sleep 2
+            
+            echo "what type off nmap scan do you wish to do?"
+            echo "-------------------------------------------"
+			echo "1) scan a ip"           
+            echo "2) scan for subnets"
+            echo "3) scan for open ports"
+            echo "4) scan for os & services"
+            echo "5) scan for ddos reflector"
+            echo "6) hearthbleed testing"
+            echo "7) return to menu"
+
+            	read nmap
+
+            if [ $nmap == 1 ];
+            	
+            	then
+
+            		echo "What ip do you wish to scan?"
+
+            			read nip
+
+            		nmap $nip
+
+            		echo "returning to nmap menu (10s)"
+
+            		sleep 10
+            		nmapm
+
+            fi
+
+            if [ $nmap == 2 ];
+            	
+            	then
+
+            		echo "what is the ip for the subnets? (ends with 0)"
+
+            			read subip
+
+            		nmap $subip/24
+            		
+            		echo "returning to nmap menu (10s)"
+
+            		sleep 10
+            		nmapm
+
+            fi
+
+            if [ $nmap == 3 ];
+            	
+            	then
+
+            		echo "what ip do you want to scan for open ports?"
+
+            			read portip
+
+            		nmap -p- $portip
+            		
+            		echo "returning to nmap menu (10s)"
+
+            		sleep 10
+            		nmapm
+
+            fi
+
+            if [ $nmap == 4 ];
+            	
+            	then
+
+            		echo "what is the ip you want to scan for os & services?"
+
+            			read osip
+
+            		nmap -A $osip
+
+            		echo "returning to nmap menu (10s)"
+
+            		sleep 10
+            		nmapm
+
+            fi
+
+            if [ $nmap == 5 ];
+            	
+            	then
+
+            		echo "being devolped"
+            		
+            		sleep 2
+
+            		echo "returning to nmap menu (10s)"
+
+            		sleep 10
+            		nmapm
+
+            fi
+
+            if [ $nmap == 6 ];
+            	
+            	then
+
+            		echo "what subnet do you want to test heartbleed? (ends with 0)"
+
+            			read hbip
+
+            		nmap -sV -p 443 --script=ssl-heartbleed $hbip/24
+
+            		echo "returning to nmap menu (10s)"
+
+            		sleep 10
+            		nmapm
+
+            fi
+
+            if [ $nmap == 7 ];
+            	
+            	then
+
+            		echo "returning to main menu"
+            		
+            		sleep 4
+            		meny
+
+            fi
+
+}
+
 ## fjerner alt tidligere i terminalen
 clear
 
@@ -25,6 +304,7 @@ mappe=/home/$USER/Desktop/4124-beta
 
 if [ -d "$mappe" ]; then
 
+	echo "${green}"
 	echo "               _                          "
 	echo "              | |                         "
 	echo " _  _  _ _____| | ____ ___  ____  _____   "
@@ -32,7 +312,7 @@ if [ -d "$mappe" ]; then
 	echo "| || || | ____| ( (__| |_| | | | | ____|  "
 	echo " \_____/|_____)\_\____\___/|_|_|_|_____)  "
 	echo "                                          "  
-	echo " $USER seems to have $beta installed!   "
+	echo " $USER ${nc} seems to have ${green} $beta ${nc} installed!   "
 	
 	sleep 5
 	clear
@@ -44,7 +324,7 @@ if [ -d "$mappe" ]; then
 
 else
  
-	echo "$beta is not installed"
+	echo "${green} $beta ${nc} is not installed"
 	sleep 2
 
 	echo "installing.."
@@ -87,6 +367,8 @@ else
 	chmod +x 4124.sh
 	sleep 1
 
+	alias 4124='/home/$USER/Desktop/4124-beta/scr/4124.sh'
+
 	cd 
 	sleep 2
 	clear
@@ -96,8 +378,8 @@ else
 	echo      "    / // _ \(_-</ __/ _  / / / / /__/ _ \/  ' \/ _ \/ / -_) __/ -_)  "
 	echo      "  /___/_//_/___/\__/\_,_/_/_/  \___/\___/_/_/_/ .__/_/\__/\__/\__/   "
 	echo      "                                            /_/                     "
-	echo "Restart from the new directory!"
-	echo "$mappe/scr/4124.sh"
+	echo "Restart the script"
+	echo "(open write 4124 in terminal)"
 
 	sleep 5
 	exit
@@ -105,7 +387,7 @@ fi
 
 ##les test.sh for info om denne delen
 
-echo "Checking $USER for SUDO pwrs.."
+echo "Checking ${green} $USER ${nc} for SUDO pwrs.."
 
 cd $mappe
 sleep 1
@@ -120,138 +402,6 @@ sleep 2
 
 cd
 
-## logo og info
-
-
-meny(){
-clear
-sleep 2
-
-echo "$green" ""
-echo "              | || |/_ |__ \| || |                "
-echo "              | || |_| |  ) | || |_               "
-echo "              |__   _| | / /|__   _|              "
-echo "                 | | | |/ /_   | |            	"
-echo "                 |_| |_|____|  |_|                "  
-echo "                 ____    ________                 "
-echo "                /_   |  /   __   \                "
-echo "                 |   |  \____    /                "
-echo "                 |   |     /    /                 "
-echo "                 |___| /\ /____/                  "
-echo "                       \/                         "
-echo ""
-echo "$blue" "                4124 script 1.9.0       "
-echo "                 Welcome Back $USER"
-
-echo "1) VPN"
-echo "2) nmap scan"
-echo "3) dir search"
-echo "4) Quit"
-}
-
-## menyen
+## går til meny void
 
 meny
-
-PS3='Choose script: '
-
-options=("VPN" "nmap scan" "dir search" "Quit")
-
-select opt in "${options[@]}"
-
-do
-    case $opt in
-
-        "VPN")
-			clear
-            echo "VPN choosen"
-            cd $mappe
-            cd kali-anonsurf
-            echo "Turn VPN off or on?"
-
-            read VPN
-
-            if [ $VPN == on ];
-            
-            then
-
-            	sudo anonsurf start
-
-            	echo "VPN is on"
-
-            	sleep 5
-
-            	meny
-
-        	else
-
-        		sudo anonsurf stop
-
-        		echo "VPN is now turned off"
-
-        		sleep 5
-
-        		meny
-
-        	fi
-
-            sleep 10
-            ;;
-
-        "nmap scan")
-			clear
-            echo "nmap choosen"
-            sleep 5
-            
-           	clear
-           	#select nmap in "${nmap[@]}"
-            
-
-            sleep 10
-
-            meny
-
-            ;;
-
-        "dir search")
-            echo "directory search is choosen"
-            sleep 2
-
-            echo "Select ip adress to scan: "
-            read IP
-
-            cd $mappe
-            cd dirsearch
-
-            python3 dirsearch.py -e dir -u $IP
-
-            sleep 10
-
-            meny
-            ;;
-
-        "Quit")
-
-			clear
-
-			echo "                                                          "
-			echo " _____         _            _                 _         _ "
-			echo "|_   ____ ___ |_|___ ___   | |___ _ _ ___ ___| |_ ___ _| |"
-			echo "  | ||  _| . || | .'|   |  | | .'| | |   |  _|   | -_| . |"
-			echo "  |_||_| |____| |__,|_|_|  |_|__,|___|_|_|___|_|_|___|___|"
-			echo "            |___|                                         ""                                                                                                                                                         "
-			echo "                   Enjoy                                  "
-
-			sleep 5
-
-			clear
-
-            exit
-            ;;
-
-        *) echo "invalid option $REPLY";;
-
-    esac
-done
-
-exit 
